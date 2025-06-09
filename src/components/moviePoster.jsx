2e51b8b5211aa32e59f4ae2a17a864e59e3024db
Muhammed Almutasim Ibrahim <www.tota.11@gmail.com>
2025-06-09 22:26:22 +0300

    feat : created movie Poster component

--- src/components/moviePoster.jsx
+++ src/components/moviePoster.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
-const moviePoster = () => {
+const MoviePoster = ({ poster, title}) => {
   return (
-    <div>moviePoster</div>
-  )
-}
+    <div className="relative w-full h-[70vh] text-white">
+      {/* Background Image */}
+      <img loading="lazy" src={poster} alt={title} className="w-full h-full object-cover rounded-lg" />
 
-export default moviePoster
+      {/* Overlay Gradient */}
+      <div className="absolute inset-0 bg-gradient-to-t from-black via-transparent to-transparent" />
+
+      {/* Content */}
+      <div className="absolute bottom-10 left-5 md:left-10 max-w-[90%]">
+        <h1 className="text-3xl md:text-5xl font-bold mb-2">{title}</h1>
+      </div>
+    </div>
+  );
+};
+
+export default MoviePoster;
